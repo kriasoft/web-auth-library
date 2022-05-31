0d314f0e78c2df6458ec1683e212fd0fd825d58a
Konstantin Tarkus <koistya@gmail.com>
2022-05-31 18:37:13 +0300

    refactor: Move Google Cloud specific stuff to `gcp` namespace

---
+++ gcp/crypto.ts
@@ -0,0 +1,53 @@
+/* SPDX-FileCopyrightText: 2022-present Kriasoft */
+/* SPDX-License-Identifier: MIT */
+
+import { base64, base64url } from "rfc4648";
+
+type KeyUsage =
+  | "encrypt"
+  | "decrypt"
+  | "sign"
+  | "verify"
+  | "deriveKey"
+  | "deriveBits"
+  | "wrapKey"
+  | "unwrapKey";
+
+/**
+ * Returns a `CryptoKey` object that you can use in the `Web Crypto API`.
+ * https://developer.mozilla.org/docs/Web/API/SubtleCrypto
+ *
+ * @example
+ *   const signingKey = await importKey(
+ *     env.GOOGLE_CLOUD_CREDENTIALS.private_key,
+ *     ["sign"],
+ *   );
+ */
+function importKey(keyData: string, keyUsages: KeyUsage[]): Promise<CryptoKey> {
+  return crypto.subtle.importKey(
+    "pkcs8",
+    base64.parse(
+      keyData
+        .replace("-----BEGIN PRIVATE KEY-----", "")
+        .replace("-----END PRIVATE KEY-----", "")
+        .replace(/\n/g, "")
+    ),
+    {
+      name: "RSASSA-PKCS1-V1_5",
+      hash: { name: "SHA-256" },
+    },
+    false,
+    keyUsages
+  );
+}
+
+/**
+ * Generates a digital signature.
+ */
+async function sign(key: CryptoKey, data: string): Promise<string> {
+  const input = new TextEncoder().encode(data);
+  const output = await self.crypto.subtle.sign(key.algorithm, key, input);
+  return base64url.stringify(new Uint8Array(output), { pad: false });
+}
+
+export { sign, importKey, KeyUsage };
---
+++ gcp/index.ts
@@ -0,0 +1,6 @@
+/* SPDX-FileCopyrightText: 2022-present Kriasoft */
+/* SPDX-License-Identifier: MIT */
+
+export { AccessToken, getAuthToken, IdToken } from "./auth.js";
+export { Credentials, getCredentials } from "./credentials.js";
+export { importKey, KeyUsage, sign } from "./crypto.js";
