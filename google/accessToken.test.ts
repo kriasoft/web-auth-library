c3ecd87b1ad10d8eafde7a2ae57beeca29a83632
Konstantin Tarkus <hello@tarkus.me>
2023-02-27 21:17:44 +0100

    refactor: Various API and implementation improvements (#9)

---
+++ google/accessToken.test.ts
@@ -0,0 +1,36 @@
+/* SPDX-FileCopyrightText: 2022-present Kriasoft */
+/* SPDX-License-Identifier: MIT */
+
+import { decodeJwt } from "jose";
+import env from "../test/env.js";
+import { getAccessToken } from "./accessToken.js";
+
+test("getAccessToken({ credentials, scope })", async () => {
+  const accessToken = await getAccessToken({
+    credentials: env.GOOGLE_CLOUD_CREDENTIALS,
+    scope: "https://www.googleapis.com/auth/cloud-platform",
+  });
+
+  expect(accessToken?.substring(0, 30)).toEqual(
+    expect.stringContaining("ya29.c.")
+  );
+});
+
+test("getAccessToken({ credentials, audience })", async () => {
+  const idToken = await getAccessToken({
+    credentials: env.GOOGLE_CLOUD_CREDENTIALS,
+    audience: "https://example.com",
+  });
+
+  expect(idToken?.substring(0, 30)).toEqual(
+    expect.stringContaining("eyJhbGciOi")
+  );
+
+  expect(decodeJwt(idToken)).toEqual(
+    expect.objectContaining({
+      aud: "https://example.com",
+      email_verified: true,
+      iss: "https://accounts.google.com",
+    })
+  );
+});
--- google/index.ts
+++ google/index.ts
@@ -1,7 +1,7 @@
 /* SPDX-FileCopyrightText: 2022-present Kriasoft */
 /* SPDX-License-Identifier: MIT */
 
-export { getAuthToken, type AccessToken, type IdToken } from "./auth.js";
-export { getCredentials, type Credentials } from "./credentials.js";
-export { importKey, sign, type KeyUsage } from "./crypto.js";
-export * as jwt from "./jwt.js";
+export * from "./accessToken.js";
+export * from "./credentials.js";
+export * from "./customToken.js";
+export * from "./idToken.js";
