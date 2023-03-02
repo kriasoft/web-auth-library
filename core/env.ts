74d56599ac0f5376312455627646270f45a25fd0
Juhani Pelli <juhani.pelli@gmail.com>
2023-03-02 11:55:10 +0200

    fix: avoid throwing if caches is undeclared

--- core/env.ts
+++ core/env.ts
@@ -1,4 +1,6 @@
 /* SPDX-FileCopyrightText: 2022-present Kriasoft */
 /* SPDX-License-Identifier: MIT */
 
-export const canUseDefaultCache = typeof caches?.default?.put === "function";
+export const canUseDefaultCache =
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  typeof (globalThis as any).caches?.default?.put === "function";
